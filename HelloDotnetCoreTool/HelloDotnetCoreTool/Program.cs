0b0940168144896f6a1790d790947bea527271ab
stulzq <stulzq@qq.com>
2018-06-02 21:38:11 +0800

    add .NET Core Global Tool demo

---
+++ HelloDotnetCoreTool/HelloDotnetCoreTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloDotnetCoreTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello .NET Core Global Tools");
+        }
+    }
+}
