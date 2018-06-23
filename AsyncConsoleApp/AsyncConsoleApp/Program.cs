82d6d05f568a3d9742357219c6dbf3d8929bdb3c
stulzq <stulzq@qq.com>
2018-06-23 22:16:09 +0800

    添加 异步控制台应用程序demo

---
+++ AsyncConsoleApp/AsyncConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("https://www.baidu.com/");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
