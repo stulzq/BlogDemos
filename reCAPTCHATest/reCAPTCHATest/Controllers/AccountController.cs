17d3842571879047b0aeb9d2e1ec2769a8de1692
stulzq <stulzq@qq.com>
2019-04-16 00:30:18 +0800

    [Add] Google reCAPTCHA

---
+++ reCAPTCHATest/reCAPTCHATest/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using reCAPTCHA.AspNetCore;
+using reCAPTCHATest.Models.Account;
+
+namespace reCAPTCHATest.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IRecaptchaService _recaptcha;
+
+        public AccountController(IRecaptchaService recaptcha)
+        {
+            _recaptcha = recaptcha;
+        }
+        // GET
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return
+            View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(AccountViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var recaptchaReault = await _recaptcha.Validate(model.GoogleToken);
+
+                if (!recaptchaReault.success || recaptchaReault.score == 0m)
+                {
+                    ModelState.AddModelError(string.Empty,"人机验证失败，请稍后重试");
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ reCAPTCHATest/reCAPTCHATest/Models/Account/AccountViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace reCAPTCHATest.Models.Account
+{
+    public class AccountViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string GoogleToken { get; set; }
+    }
+}
