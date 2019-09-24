dcb0f562f2f41b688af07722969c11d5a3489b50
stulzq <stulzq@qq.com>
2019-09-24 23:43:33 +0800

    [Add] AspNetCoreGrpc

---
+++ AspNetCoreGrpc/src/AspNetCoreGrpcClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using AspNetCoregRpcService;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+
+namespace AspNetCoreGrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "晓晨" });
+            Console.WriteLine("调用Greeter服务 : " + reply.Message);
+
+            var catClient = new LuCat.LuCatClient(channel);
+            var catReply = await catClient.SuckingCatAsync(new Empty());
+            Console.WriteLine("调用撸猫服务："+ catReply.Message);
+            Console.ReadKey();
+        }
+    }
+}
